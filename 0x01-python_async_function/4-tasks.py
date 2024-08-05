72e9c5023107e6bbb9be4301a0ee4991717f5685
ali-hy <ali.haitham.ymy@gmail.com>
2024-08-05 23:52:55 +0400

    4-tasks.py

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''measure the runtime of the function fomr wait_n'''
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''run n wait_randoms at the same time'''
+    delays = [task_wait_random(max_delay) for _ in range(n)]
+    delays = asyncio.as_completed(delays)
+    return [await delay for delay in delays]
+
+n = 5
+max_delay = 6
+print(asyncio.run(task_wait_n(n, max_delay)))
