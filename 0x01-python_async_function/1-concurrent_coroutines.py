7534595e59f14ed8e5f2c76261d0fc0af6b9306e
ali-hy <ali.haitham.ymy@gmail.com>
2024-08-05 22:48:05 +0400

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 '''run multiple wait_randoms at the same time'''
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     '''run n wait_randoms at the same time'''
     delays = [wait_random(max_delay) for _ in range(n)]
-    return sorted([await delay for delay in delays])
+    delays = asyncio.as_completed(delays)
+    return [await delay for delay in delays]
