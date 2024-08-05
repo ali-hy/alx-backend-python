06e99e7d6eef48eba9e57ddf4074b4942e932cbf
ali-hy <ali.haitham.ymy@gmail.com>
2024-08-05 22:30:22 +0400

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,5 +5,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     '''run n wait_randoms at the same time'''
-    delays = [wait_random(max_delay) for _ in range(n)]
+    delays = sorted([wait_random(max_delay) for _ in range(n)])
     return [await delay for delay in delays]
